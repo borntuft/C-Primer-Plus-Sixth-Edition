c639df8fa5c311065718cc37a71821bc125f9147
Borntuft <32026074+borntuft@users.noreply.github.com>
2019-06-19 19:54:23 -0400

    Add files via upload

---
+++ 4-7.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define PAGES 1
+
+int main (void)
+{
+
+    printf("*%d*\n", PAGES);
+    printf("*%2d*\n", PAGES);
+    printf("*%10d*\n", PAGES);
+    printf("*%-10d*\n", PAGES);
+
+    return 0;
+}
+
